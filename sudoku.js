52b8379f58db8c8505bc61e21ede8c2dce61f29f
Jayden Ramirez <jaydenr0311@gmail.com>
2025-07-31 20:41:10 -0400

    Changes:
    -Made it so you can select and unselect the same number on the bottom repeatedly
    -Added a backtracking solver to generate random boards
    By adding the random board generator I took out the actual game functionality but I'll fix it tomorrow

--- sudoku.js
+++ sudoku.js
@@ -1,20 +1,63 @@
+/*changes:
+-Made it so you can select and unselect the same number on the bottom repeatedly
+-Added a backtracking solver to generate random boards
+*/
+
 var numSelected = null;
 var prevTileSelected = null;
 var tileSelected = null;
 
 var errors = 0;
 
-var board = [
-    "273-94-16",
-    "68-273--9",
-    "--51862--",
-    "---3--78-",
-    "-----9-63",
-    "83-7---24",
-    "-694-83-2",
-    "7-26--198",
-    "318-27--5"
-]
+//https://stackoverflow.com/questions/6924216/how-to-generate-sudoku-boards-with-unique-solutions
+function generateBoard()    {
+    const board = Array(9).fill().map(() => Array(9).fill(0));
+    fillBoard(board);
+    return board;
+}
+function fillBoard(board, row = 0, col = 0) {
+    if(row==9) return true;  //base case
+    if(col==9) return fillBoard(board, row+1, 0);
+    if(board[row][col] != 0) return fillBoard(board, row, col+1);
+
+    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for(let i = nums.length - 1; i > 0; i--)    {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    for(const num of nums) {
+        if(isValid(board, row, col, num))   {
+            board[row][col] = num;
+            if(fillBoard(board, row, col+1)) return true;    //recurse
+            board[row][col] = 0;    //backtrack
+        }
+    }
+    return false;
+}
+function isValid(board, row, col, num)  {
+    //row check
+    for(let c=0; c<9; c++)  {
+        if (board[row][c] == num) return false;
+    }
+    //column check
+    for(let r=0; r<9; r++)  {
+        if (board[r][col] == num) return false;
+    }
+    //3x3 check
+    const boxRow = Math.floor(row / 3) * 3;
+    const boxCol = Math.floor(col / 3) * 3;
+    for (let r=0; r<3; r++) {
+        for (let c = 0; c < 3; c++) {
+            if (board[boxRow + r][boxCol + c] == num) return false;
+        }
+    }
+    return true;
+}
+
+var board;
 
 var solution = [
     "273594816",
@@ -29,6 +72,8 @@ var solution = [
 ]
 
 window.onload = function() {
+    board = generateBoard();
+    console.log(board);
     setGame();
 }
 
@@ -70,7 +115,11 @@ function selectNumber() {
     if(numSelected!=null)   {
         numSelected.classList.remove("number-selected");
     }
-    numSelected = this;
+    if(numSelected == this)  {
+        numSelected = null;
+        return;
+    }
+    numSelected = this;   
     numSelected.classList.add("number-selected");
     //document.getElementById("h1").innerHTML= "yurp";  doesnt work idk why
 }
